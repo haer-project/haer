07ba4b4438ff43e738fea50d28a10a7e387508ae
佐藤 希 <sato@i-fc.jp>
2016-08-17 23:28:06 +0900

    初回アクセスのみドアのイラストを表示する js を追加 #13

--- htdocs/haer-template/js/script.js
+++ htdocs/haer-template/js/script.js
@@ -1,55 +1,71 @@
-document.body.style.display = 'none';
+// document.body.style.display = 'none';
 $(function(){
     var fadeSpeed = 1000;
 
-    // TODO:初回アクセスのみドアのイラストを表示する
-    // if($.cookie('access')){
-    //     $('.haer-view-02').css({display:'none'});
-    // }
-    // $(window).load(function(){
-    //     $.cookie('access',$('body').addClass('access'))
-    // })
-    $('.haer-view-02').hide();
-    $('.haer-view-01').fadeIn(fadeSpeed);
-    $('.haer-view-01').on('click',function(){
-        $('.haer-view-01').fadeOut(fadeSpeed,'swing');
-        $('.haer-view-02').fadeIn(fadeSpeed,'swing');
-    });
+    // 初回アクセスのみドアのイラストを表示する
+    var firstAccess = function(){
+        var $access = $.cookie('access');
+        if($access !== 'ok'){
+            $.cookie('access', 'ok', { expires: 1 });
+            $('.haer-view-02').hide();
+            $('.haer-view-01').fadeIn(fadeSpeed);
+            $('.haer-view-01').on('click',function(){
+                $('.haer-view-01').fadeOut(fadeSpeed,'swing');
+                $('.haer-view-02').fadeIn(fadeSpeed,'swing');
+            });
+            console.log('ご訪問ありがとうございます！')
+        } else {
+            $('.haer-view-02').fadeIn(fadeSpeed,'swing');
+            console.log('2回めのご訪問ありがとうございます！')
+        }
+    }
+    if($('.haer-view-01').length) firstAccess();
+
 
     // ページをフェードインする
-    $('body').fadeIn(fadeSpeed);
-    $(window).on('beforeunload', function(e){
-        $('body').fadeOut(fadeSpeed);
-    })
+    // var fadeInContents = function(){
+    //     $('body').fadeIn(fadeSpeed);
+    //     $(window).on('beforeunload', function(e){
+    //         $('body').fadeOut(fadeSpeed);
+    //     })
+    // }();
 
     // 少しスクロールしたら header を小さくする
-    $(window).on('scroll',function(){
-        if($(this).scrollTop() > 10){
-            $('header').addClass('is-small');
-        } else {
-            $('header').removeClass('is-small');
-        }
-    });
+    var scrollSmallHeader = function(){
+        $(window).on('scroll',function(){
+            if($(this).scrollTop() > 10){
+                $('header').addClass('is-small');
+            } else {
+                $('header').removeClass('is-small');
+            }
+        });
+    }();
 
     // グロナビのカレント表示
-    var $bodyId = $('header + div').attr('id');
-    $('header .haer-nav-global a').each(function(){
-        var $navClass = $(this).attr('class');
-        if($navClass == $bodyId ){
-            $(this).addClass('is-current');
-        } else {
-            $(this).removeClass('is-current');
-        }
-    });
+    var addCurrent = function(){
+        var $bodyId = $('header + div').attr('id');
+        $('header .haer-nav-global a').each(function(){
+            var $navClass = $(this).attr('class');
+            if($navClass == $bodyId ){
+                $(this).addClass('is-current');
+            } else {
+                $(this).removeClass('is-current');
+            }
+        });
+    }();
 
     //画像の右クリックを禁止する
-    $('img').on('contextmenu',function(){
-        return false;
-    })
+    var saveBlockImage = function(){
+        $('img').on('contextmenu',function(){
+            return false;
+        })
+    }();
 
     // アコーディオン
-    $('.js-accordion').next().hide();
-    $('.js-accordion').on('click', function(){
-        $(this).next().slideToggle();
-    });
+    var accordion = function(){
+        $('.js-accordion').next().hide();
+        $('.js-accordion').on('click', function(){
+            $(this).next().slideToggle();
+        });
+    }();
 })
